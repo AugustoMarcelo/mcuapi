7027388ce93c9c7e1897af8bb62741838dde23c8
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-17 17:23:57 -0300

    Fixing missing create implementation

--- src/modules/movies/infra/http/controllers/MoviesController.ts
+++ src/modules/movies/infra/http/controllers/MoviesController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateMovieService from '@modules/movies/services/CreateMovieService';
 
 export default class MoviesController {
-  public async create(rquest: Request, response: Response): Promise<Response> {
-    return response.json();
+  public async create(request: Request, response: Response): Promise<Response> {
+    const createMovie = container.resolve(CreateMovieService);
+
+    const movie = await createMovie.execute(request.body);
+
+    return response.json(movie);
   }
 }
