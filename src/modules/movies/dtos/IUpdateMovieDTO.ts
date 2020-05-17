a61f145af130353a55d136e8b6347d96f8d6130e
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-17 19:34:01 -0300

    DTO for update movie

---
+++ src/modules/movies/dtos/IUpdateMovieDTO.ts
@@ -0,0 +1,14 @@
+export default interface IUpdateMovieDTO {
+  movie_id: string;
+  title?: string;
+  release_date?: Date;
+  box_office?: number;
+  duration?: number;
+  overview?: string;
+  cover_url?: string;
+  directed_by?: string;
+  phase?: number;
+  saga?: string;
+  chronology?: number;
+  post_credit_scenes?: number;
+}
