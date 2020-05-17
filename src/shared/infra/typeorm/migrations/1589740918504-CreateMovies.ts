1db12a3cc7e83efc6563c98e3dfb31c6e69d630b
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-17 17:24:31 -0300

    Created migration for movies table

---
+++ src/shared/infra/typeorm/migrations/1589740918504-CreateMovies.ts
@@ -0,0 +1,77 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateMovies1589740918504 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'movies',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'release_date',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'box_office',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'duration',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'overview',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cover_url',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'directed_by',
+            type: 'varchar',
+          },
+          {
+            name: 'phase',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'saga',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'chronology',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'post_credit_scenes',
+            type: 'int',
+            default: 0,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('movies');
+  }
+}
