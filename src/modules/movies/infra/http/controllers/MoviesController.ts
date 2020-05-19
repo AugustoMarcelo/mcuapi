3ce60618d7389eb1c1fe8d7bdaea87b66ff98558
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-19 19:08:22 -0300

    Created routes to list and show movies with pagination

--- src/modules/movies/infra/http/controllers/MoviesController.ts
+++ src/modules/movies/infra/http/controllers/MoviesController.ts
@@ -3,8 +3,27 @@ import { container } from 'tsyringe';
 
 import CreateMovieService from '@modules/movies/services/CreateMovieService';
 import UpdateMovieService from '@modules/movies/services/UpdateMovieService';
+import ListAllMoviesService from '@modules/movies/services/ListAllMoviesService';
+import ShowMovieService from '@modules/movies/services/ShowMovieService';
+
+interface IRequestQuery {
+  page?: number;
+  limit?: number;
+}
 
 export default class MoviesController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { page, limit }: IRequestQuery = request.query;
+
+    const listAllMovies = container.resolve(ListAllMoviesService);
+    const { data, total } = await listAllMovies.execute({
+      page,
+      limit,
+    });
+
+    return response.json({ data, total });
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const createMovie = container.resolve(CreateMovieService);
 
@@ -24,5 +43,15 @@ export default class MoviesController {
     });
 
     return response.json(updatedMovie);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { movie_id } = request.params;
+
+    const showMovie = container.resolve(ShowMovieService);
+
+    const movie = await showMovie.execute({ movie_id });
+
+    return response.json(movie);
   }
 }
--- src/modules/movies/infra/http/routes/movies.routes.ts
+++ src/modules/movies/infra/http/routes/movies.routes.ts
@@ -5,7 +5,9 @@ import MoviesController from '../controllers/MoviesController';
 const moviesRouter = Router();
 const moviesController = new MoviesController();
 
+moviesRouter.get('/', moviesController.index);
 moviesRouter.post('/', moviesController.create);
 moviesRouter.put('/:movie_id', moviesController.update);
+moviesRouter.get('/:movie_id', moviesController.show);
 
 export default moviesRouter;
