64afed82805644c11821c87e5de2dc3660bb9474
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-19 19:25:36 -0300

    Updated route to /api/v1 version

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,6 @@ import moviesRouter from '@modules/movies/infra/http/routes/movies.routes';
 
 const routes = Router();
 
-routes.use('/movies', moviesRouter);
+routes.use('/api/v1/movies', moviesRouter);
 
 export default routes;
