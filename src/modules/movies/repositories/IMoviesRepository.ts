391c779d06e3de370b55031bb926a20612b18e78
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-19 19:10:13 -0300

    Added method findAll

--- src/modules/movies/repositories/IMoviesRepository.ts
+++ src/modules/movies/repositories/IMoviesRepository.ts
@@ -1,8 +1,11 @@
 import IMovie from '../entities/IMovie';
 import ICreateMovieDTO from '../dtos/ICreateMovieDTO';
+import IFindAllMoviesDTO from '../dtos/IFindAllMoviesDTO';
+import IFindAllMoviesResponseDTO from '../dtos/IFindAllMoviesResponseDTO';
 
 export default interface IMoviesRepository {
   create(data: ICreateMovieDTO): Promise<IMovie>;
   update(movie: IMovie): Promise<IMovie>;
   findById(id: string): Promise<IMovie | undefined>;
+  findAll(data: IFindAllMoviesDTO): Promise<IFindAllMoviesResponseDTO>;
 }
