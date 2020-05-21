aae7731bc7e33e14c765107b14a5bd4a81bf525d
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-21 19:51:47 -0300

    Implemented hash provider with bcryptjs

---
+++ src/modules/users/providers/index.ts
@@ -0,0 +1,5 @@
+import { container } from 'tsyringe';
+import IHashProvider from './HashProvider/models/IHashProvider';
+import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
+
+container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
