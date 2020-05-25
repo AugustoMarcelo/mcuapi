3a9f4dae766d53caf0a9f4d56433f9ab85b590ba
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-25 20:50:15 -0300

    Created configs for production and dev

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+const dev =  {
   type: "postgres",
   host: process.env.DB_HOST,
   port: 5432,
@@ -6,7 +6,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: [
-    "./src/modules/**/infra/typeorm/entities/*.ts"
+    "./src/modules/**/infra/typeorm/entities/*.ts",
   ],
   migrations: [
     "./src/shared/infra/typeorm/migrations/*.ts"
@@ -15,3 +15,23 @@ module.exports = {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
 }
+
+const production =  {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [
+    "./dist/modules/**/infra/typeorm/entities/*.js",
+  ],
+  migrations: [
+    "./dist/shared/infra/typeorm/migrations/*.js"
+  ],
+  cli: {
+    "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+  }
+}
+
+module.exports = process.env.NODE_ENV === 'production' ? production : dev;
