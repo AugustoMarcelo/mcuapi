f6e631feb1f8ad2b42997155ffb5f4dfd5731e7f
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-25 20:54:59 -0300

    Ignored dist folder

--- dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: process.env.JWT_SECRET || 'default',
-        expiresIn: '1d',
-    },
-};
--- dist/modules/users/providers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var tsyringe_1 = require("tsyringe");
-var BCryptHashProvider_1 = __importDefault(require("./HashProvider/implementations/BCryptHashProvider"));
-tsyringe_1.container.registerSingleton('HashProvider', BCryptHashProvider_1.default);
