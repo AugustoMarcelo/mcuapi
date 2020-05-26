f7a488187af8ff88d360a8fe99f8f86b875c40d7
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-25 21:14:35 -0300

    Setting port dynamically

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -28,6 +28,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('🦸‍♂️ api running on port 3333');
 });
