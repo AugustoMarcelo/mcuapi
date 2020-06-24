2e99b59702fd15f4513b2ee58e123a93baf24155
AugustoMarcelo <mrclgst10@gmail.com>
2020-06-23 21:16:43 -0300

    Added tests for movies attributes filter

--- src/modules/movies/services/ListAllMoviesService.spec.ts
+++ src/modules/movies/services/ListAllMoviesService.spec.ts
@@ -53,4 +53,35 @@ describe('ListAllMovies', () => {
     expect(data).toHaveLength(1);
     expect(total).toBe(3);
   });
+
+  it('Should be able to list only movies attributes with columns passed by query params', async () => {
+    fakeMoviesRepository.create({
+      title: 'Iron Man',
+      release_date: new Date(),
+      box_office: 585171547,
+      duration: 126,
+      overview:
+        "2008's Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity. When he uncovers a nefarious plot with global implications, he dons his powerful armor and vows to protect the world as Iron Man.",
+      cover_url:
+        'https://raw.githubusercontent.com/AugustoMarcelo/mcuapi/master/covers/iron-man.jpg',
+      directed_by: 'Jon Favreau',
+      phase: 1,
+      saga: 'Infinity Saga',
+      chronology: 3,
+      post_credit_scenes: 1,
+    });
+
+    const { data } = await listAllMovies.execute({
+      columns: 'title,release_date,cover_url',
+    });
+
+    expect(data[0]).not.toHaveProperty('box_office');
+    expect(data[0]).not.toHaveProperty('duration');
+    expect(data[0]).not.toHaveProperty('overview');
+    expect(data[0]).not.toHaveProperty('directed_by');
+    expect(data[0]).not.toHaveProperty('phase');
+    expect(data[0]).not.toHaveProperty('saga');
+    expect(data[0]).not.toHaveProperty('chronology');
+    expect(data[0]).not.toHaveProperty('post_credit_scenes');
+  });
 });
