99d04abf6c5cfd74ed5cd1acf33584e03e59f4c6
AugustoMarcelo <mrclgst10@gmail.com>
2020-08-23 20:55:31 -0300

    :fire: removed uuid column strategy generation

--- src/shared/infra/typeorm/migrations/1589740918504-CreateMovies.ts
+++ src/shared/infra/typeorm/migrations/1589740918504-CreateMovies.ts
@@ -8,10 +8,8 @@ export default class CreateMovies1589740918504 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'uuid',
+            type: 'int',
             isPrimary: true,
-            generationStrategy: 'uuid',
-            default: 'uuid_generate_v4()',
           },
           {
             name: 'title',
