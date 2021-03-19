794ac7aa92f9173bce79cc6faf926af73afb2815
Marcelo <mrclgst10@gmail.com>
2021-03-19 16:00:55 -0300

    feat: add tvshows route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import moviesRouter from '@modules/movies/infra/http/routes/movies.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import tvshowsRouter from '@modules/tvshows/infra/http/routes/tvshows.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/api/v1/movies', moviesRouter);
+routes.use('/api/v1/tvshows', tvshowsRouter);
 
 export default routes;
