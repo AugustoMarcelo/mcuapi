6bac9bc3105ac8c012b31ab210bb3375d7655686
Marcelo <mrclgst10@gmail.com>
2021-03-19 16:00:14 -0300

    chore: register singleton creation to tvshow repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,8 +4,15 @@ import '@modules/users/providers';
 
 import IMoviesRepository from '@modules/movies/repositories/IMoviesRepository';
 import MoviesRepository from '@modules/movies/infra/typeorm/repositories/MoviesRepository';
+import ITVShowsRepository from '@modules/tvshows/repositories/ITVShowsRepository';
+import TVShowsRepository from '@modules/tvshows/infra/typeorm/repositories/TVShowsRepository';
 
 container.registerSingleton<IMoviesRepository>(
   'MoviesRepository',
   MoviesRepository,
+);
+
+container.registerSingleton<ITVShowsRepository>(
+  'TVShowsRepository',
+  TVShowsRepository,
 );
