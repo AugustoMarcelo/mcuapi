881a451135a39bafacc64d9177eef004739bcd80
Marcelo <mrclgst10@gmail.com>
2021-03-19 15:56:10 -0300

    test: mock to create tvshow

---
+++ src/modules/tvshows/utils/mockTVShow.ts
@@ -0,0 +1,19 @@
+import ITVShow from '@modules/tvshows/entities/ITVShow';
+import faker from 'faker';
+
+const mockTVShow = (): ITVShow => ({
+  id: faker.random.number(),
+  title: faker.random.words(),
+  overview: faker.random.words(),
+  cover_url: faker.internet.url(),
+  trailer_url: faker.internet.url(),
+  number_episodes: faker.random.number(),
+  number_seasons: faker.random.number(),
+  directed_by: faker.name.findName(),
+  phase: faker.random.number(),
+  saga: faker.random.words(),
+  release_date: faker.date.past(),
+  last_aired_date: faker.date.future(),
+});
+
+export default mockTVShow;
