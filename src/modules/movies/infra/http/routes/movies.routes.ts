4712f9117ee3aa1654075f53c464545502774d6d
Marcelo <mrclgst10@gmail.com>
2021-05-06 09:02:34 -0300

    refactor: removed post and put methods

--- src/modules/movies/infra/http/routes/movies.routes.ts
+++ src/modules/movies/infra/http/routes/movies.routes.ts
@@ -1,14 +1,11 @@
 import { Router } from 'express';
 
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import MoviesController from '../controllers/MoviesController';
 
 const moviesRouter = Router();
 const moviesController = new MoviesController();
 
 moviesRouter.get('/', moviesController.index);
-moviesRouter.post('/', ensureAuthenticated, moviesController.create);
-moviesRouter.put('/:movie_id', ensureAuthenticated, moviesController.update);
 moviesRouter.get('/:movie_id', moviesController.show);
 
 export default moviesRouter;
