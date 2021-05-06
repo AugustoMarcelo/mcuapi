a824305a93b66e069e295c425af48286b1eec676
Marcelo <mrclgst10@gmail.com>
2021-05-06 09:01:11 -0300

    refactor: removed unused users module

--- src/@types/express.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    user: {
-      id: string;
-    };
-  }
-}
--- src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: process.env.JWT_SECRET || 'default',
-    expiresIn: '1d',
-  },
-};
