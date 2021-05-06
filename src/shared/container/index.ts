178c0e2cd35b0112ca39fa44864f68e092703378
Marcelo <mrclgst10@gmail.com>
2021-05-06 09:03:15 -0300

    refactor: removed unused users providers module import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,5 @@
 import { container } from 'tsyringe';
 
-import '@modules/users/providers';
-
 import IMoviesRepository from '@modules/movies/repositories/IMoviesRepository';
 import MoviesRepository from '@modules/movies/infra/typeorm/repositories/MoviesRepository';
 import ITVShowsRepository from '@modules/tvshows/repositories/ITVShowsRepository';
