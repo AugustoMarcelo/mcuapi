873d930061c390129fd5497c330e16190993ca01
Marcelo <mrclgst10@gmail.com>
2022-11-17 10:35:04 -0300

    create movie streaming interface and typeorm implementation

---
+++ src/modules/streamings/entities/IMovieStreaming.ts
@@ -0,0 +1,6 @@
+export default interface IMovieStreaming {
+  id?: number;
+  movie_id: number;
+  platform: string;
+  url: string;
+}
---
+++ src/modules/streamings/infra/typeorm/entities/MovieStreaming.ts
@@ -0,0 +1,30 @@
+import Movie from '@modules/movies/infra/typeorm/entities/Movie';
+import IMovieStreaming from '@modules/streamings/entities/IMovieStreaming';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('movie_streamings')
+class MovieStreaming implements IMovieStreaming {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  movie_id: number;
+
+  @ManyToOne(() => Movie, movie => movie.streamings)
+  @JoinColumn({ name: 'movie_id' })
+  movie: Movie;
+
+  @Column()
+  platform: string;
+
+  @Column()
+  url: string;
+}
+
+export default MovieStreaming;
